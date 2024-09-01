ac78d32a2f5bc6824e47d7ffc62cd209fed31213
Vanny2402 <79197656+Vanny2402@users.noreply.github.com>
2024-09-01 17:29:50 +0700

    end of local

---
+++ src/main/java/getcapability/config/WebConfig.java
@@ -0,0 +1,18 @@
+package getcapability.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*") // You can specify the allowed origins or use "*" for any origin
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
--- src/main/java/getcapability/device/controller/DeviceController.java
+++ src/main/java/getcapability/device/controller/DeviceController.java
@@ -3,6 +3,7 @@ package getcapability.device.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +17,7 @@ public class DeviceController {
     private CapabalityService capabilityService;
 
     @GetMapping("/adbDevice")
+    @CrossOrigin(origins = "*")
     public List<CapabilityModel> getDeviceCapabilities() {
         return capabilityService.getConnectedDevices();
     }
